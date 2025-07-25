5a78ff22ce6f07d2f68038544a499e3076ee3f2e
Amethyste34 <laurence.amethyste@gmail.com>
2025-07-25 16:49:28 +0200

    TP01 jusqu'à la fin

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,15 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        // Titre en ASCII grâce à jfiglet
+        String titreAscii = FigletFont.convertOneLine("APP");
+        System.out.println(titreAscii);
         System.out.println("Hello World!");
     }
 }
