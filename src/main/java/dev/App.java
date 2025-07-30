ecd096aca0e1e149f9e2a3f3e89082b84b4c8ce8
Amethyste34 <laurence.amethyste@gmail.com>
2025-07-30 11:27:19 +0200

    TP03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -2,14 +2,18 @@ package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
 
-/**
- * Hello world!
- */
+import java.util.ResourceBundle;
+
 public class App {
     public static void main(String[] args) {
+        String titre = ResourceBundle.getBundle("application").getString("titre");
         // Titre en ASCII grâce à jfiglet
-        String titreAscii = FigletFont.convertOneLine("APP");
+        String titreAscii = FigletFont.convertOneLine(titre);
         System.out.println(titreAscii);
-        System.out.println("Hello World!");
+
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString(
+                "environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
